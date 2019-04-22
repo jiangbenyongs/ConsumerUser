0ae05fa1f394e6c1790cdc2135f6348bb1e335dc
github <jiangbenyongs@163.com>
2019-04-22 23:54:37 +0800

    regist eurake

--- src/main/java/com/xj/cloud/ConsumerUserApplication.java
+++ src/main/java/com/xj/cloud/ConsumerUserApplication.java
@@ -2,9 +2,11 @@ package com.xj.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConsumerUserApplication {
 	@Bean
