1b224bf9c5f98e239a7ec654d5989c4a014185b8
github <jiangbenyongs@163.com>
2019-04-30 20:20:19 +0800

    add log4j

--- src/main/java/com/xj/cloud/controller/ConsController.java
+++ src/main/java/com/xj/cloud/controller/ConsController.java
@@ -1,6 +1,11 @@
 package com.xj.cloud.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +15,23 @@ import com.xj.cloud.pojo.User;
 
 @RestController
 public class ConsController {
-
+	private Logger log = LoggerFactory.getLogger(ConsController.class);
+	
 	@Autowired
 	private RestTemplate res;
+	@Autowired
+	private LoadBalancerClient lbc;
 
 	@GetMapping("/user/{id}")
 	public User findById(@PathVariable Long id) {
+		
+		return res.getForObject("http://ProviderUser/" + id, User.class);
 
-		return res.getForObject("http://localhost:8009/" + id, User.class);
-
+	}
+	
+	@GetMapping("/log-user-instance")
+	public void logUserInstance() {
+		ServiceInstance serviceIns = lbc.choose("ProviderUser");
+		log.info("{}:{}:{}",serviceIns.getServiceId(),serviceIns.getHost(),serviceIns.getPort());
 	}
 }
